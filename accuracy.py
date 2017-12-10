a60dc8ab657f8ba365e7a414056ebb6238f1f145
Archie <archie.isdiningrat@bizzy.co.id>
2017-12-10 21:03:18 +0700

    fix accuracy

--- accuracy.py
+++ accuracy.py
@@ -1,6 +1,7 @@
 import xlrd as xr
 from libs import utils as u
 from operator import itemgetter
+import copy
 
 file = "archive/dataset.xlsx"
 
@@ -11,8 +12,8 @@ sheet_train = workbook.sheet_by_index(0)
 train_data = u.load_data(sheet_train)
 
 # load data test
-start_index = 1000
-test_data = train_data[start_index:4000]
+start_index = 3000
+test_data = copy.deepcopy(train_data[start_index:4000])
 
 # initiate k, make it even
 k = 11
@@ -35,7 +36,7 @@ for test in test_data:
 n_data = len(test_data)
 true_prediction = 0
 for i in range(n_data):
-    if result[i]['hoax'] == train_data[start_index + i]['hoax']:
+    if int(result[i]['hoax']) == int(train_data[start_index + i]['hoax']):
         true_prediction += 1
 
 acc = (100 / n_data) * true_prediction
